fae3a1907c2bae24af8a05b53875d7d3aaac3caa
90553 <90553@DESKTOP-6QAKKSF>
2023-11-15 23:58:18 +0300

    Add project files.

---
+++ İlk_Projem_Hesap_Makinem/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace İlk_Projem_Hesap_Makinem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi1, sayi2, sonuc;
+            char operatör;
+
+            Console.WriteLine("Hesap Makinesi");
+            Console.Write("Birinci sayıyı girin: ");
+            sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı girin: ");
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("İşlemi seçin (+, -, *, /): ");
+            operatör = Convert.ToChar(Console.ReadLine());
+
+            switch (operatör)
+            {
+                case '+':
+                    sonuc = sayi1 + sayi2;
+                    Console.WriteLine("Sonuç: " + sonuc);
+                    break;
+                case '-':
+                    sonuc = sayi1 - sayi2;
+                    Console.WriteLine("Sonuç: " + sonuc);
+                    break;
+                case '*':
+                    sonuc = sayi1 * sayi2;
+                    Console.WriteLine("Sonuç: " + sonuc);
+                    break;
+                case '/':
+                    if (sayi2 != 0)
+                    {
+                        sonuc = sayi1 / sayi2;
+                        Console.WriteLine("Sonuç: " + sonuc);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hata: Sıfıra bölme hatası!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz operatör girişi!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
